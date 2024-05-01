c604c29e48ebd0d7ba943ae0365300bedf697900
298A-E9E3 <166412009+298A-E9E3@users.noreply.github.com>
2024-05-01 15:04:23 -0400

    added stuff

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function send(){
     var bodyBox = document.getElementById("body");
     const script = "https://script.google.com/a/macros/fcpsschools.net/s/AKfycbwAC6Oaozf2Qta8I2I1rlCLeIhIww50EXG0TAq82C93KawKmveOcJz-x-y5gawbX_CP_A/exec"
     open(script+"?method="+addressBox.value+"|"+subjectBox.value+"|"+bodyBox.value);
-
-
+}
+function decrypt(){
+    var inputBox = document.getElementById("unencrypt")
 }
